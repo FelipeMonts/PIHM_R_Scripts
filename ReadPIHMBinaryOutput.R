2f94a0ab1e119252b859b9eebbe51e816007394c
FelipeMonts <frm10@psu.edu>
2017-11-21 15:05:22 -0500

    imporved the names of generic files and added the output plotting capabilities of PHIM_Maps.R

--- ReadPIHMBinaryOutput.R
+++ ReadPIHMBinaryOutput.R
@@ -1,7 +1,7 @@
 #############################################################################################################################
 #
 #  Program read binary files from the phim output.
-#  Based on the Matlab program that Yubibg developed for the same process
+#  Based on the Matlab program that Yuning developed for the same process
 #   
 #   function [time, var] = pihm_read_output_func (filename, nvar, elem)
 # 
@@ -102,7 +102,14 @@ TimeStart<-Sys.time()  ;
 #  Set Working directory
 
 
-setwd("C:/Felipe/PIHM-CYCLES/PIHM/PIHM_Felipe/CNS/WE-38/WE38_Files_PIHM_Cycles20170208/SWATPIHMRcode") ; 
+setwd("C:/Felipe/PIHM-CYCLES/PIHM/PIHM_R_Scripts/MM_PIHM_outputs") ; 
+
+############### Create a project directory where all processed results will be stored#############
+
+Output.Project<-c("HansYoust.1711211109")  ;
+
+dir.create(paste0("./",Output.Project)) ;
+####
 
 
 ###############################################################################################################
@@ -119,18 +126,18 @@ library("hexView") ;
 ###############################################################################################################
 #                      Read binary files 
 ###############################################################################################################
-list.files("../")   ;
+list.files(paste0("./",Output.Project))   ;
 
-file_info<-file.info("../WE38.recharge.dat")   ; # information about the binary file
+file_info<-file.info(paste0('./',Output.Project,'/', 'HansYoust.recharge.dat'))   ; # information about the binary file
 
 word.size=8 ;  # eight bytes per word
 
-Triangle.no=883 ; # number of triangles in the output simulation
+Triangle.no=NumEle ; # number of triangles in the output simulation
 
 No.Rows<-file_info$size/word.size/(Triangle.no+1) ; # number of rows obtained by dividing the file size in bytes
 # by wordsize*(Tiangle.no +1 (time stamp))  
 
-Read_data<-readBin("../WE38.recharge.dat" , "numeric" , size=word.size, n=file_info$size/word.size) ; # read the binary filw
+Read_data<-readBin(paste0('./',Output.Project,'/', 'HansYoust.recharge.dat') , "numeric" , size=word.size, n=file_info$size/word.size) ; # read the binary filw
 
 Recharge<-as.data.frame(matrix(data=Read_data, ncol=Triangle.no+1, byrow=T)); # create a matrix with the binary file
 # and then transforme it into a dataframe.
@@ -138,5 +145,310 @@ Recharge<-as.data.frame(matrix(data=Read_data, ncol=Triangle.no+1, byrow=T)); # 
 
 Recharge$Time<-as.POSIXct(Recharge[,1], origin="1970-01-01", tz="UTC") ;
 
+
+##############################Program to plot the messh files in PIHM
+##############################Based on the Book SpatialData analyisis in R
+####    Preeeliminaries
+
+
+
+
+
+########### Call the library packages needed for the program to work #############
+
+
+
+library(rgdal);
+
+library(sp);
+
+library(RColorBrewer) ;
+
+library(lattice) ;
+
+library(ggplot2)  ;
+
+library(rgeos)   ;
+
+library(tmap) ;
+
+library(dplyr)  ;
+
+library(tidyr)  ;
+
+
+########### Read infromation about the shape files ###########
+
+Output.Project.mesh.info<-ogrInfo("C:/Felipe/PIHM-CYCLES/PIHM/PIHM_Felipe/CNS/Manhantango/HydroTerreFullManhantango/HansYostDeepCreek/Aug2920171550/3DomainDecomposition/MergeVectorLayer000_q30_a200000.shp");
+
+
+### The number of polygons in the shape file is WE38.mesh.info$nrows
+
+Output.Project.mesh.info$nrows
+
+
+##### Read the shape files form the shape files used in preparation of the mesh file #######
+
+Output.Project.mesh<-readOGR("C:/Felipe/PIHM-CYCLES/PIHM/PIHM_Felipe/CNS/Manhantango/HydroTerreFullManhantango/HansYostDeepCreek/Aug2920171550/3DomainDecomposition/MergeVectorLayer000_q30_a200000.shp" ); 
+
+### The Shape file polygons ID are from 0 to n, therefore would not match the elements ID in PIHM. 
+### To change that the polygon Ids can be changed to make them match
+
+for ( i  in 1:Output.Project.mesh.info$nrows) {
+  
+  Output.Project.mesh@polygons[[i]]@ID<-as.character(i)
+}
+
+###### Similarly for the river shape File
+
+Output.Project.river.info<-ogrInfo('C:/Felipe/PIHM-CYCLES/PIHM/PIHM_Felipe/CNS/Manhantango/HydroTerreFullManhantango/HansYostDeepCreek/Aug2920171550/2VectorProcessing/Stream10000_sln30_xln_Decomp.shp' )    ;
+
+
+Output.Project.river.info$nrows
+
+
+Output.Project.river<-readOGR('C:/Felipe/PIHM-CYCLES/PIHM/PIHM_Felipe/CNS/Manhantango/HydroTerreFullManhantango/HansYostDeepCreek/Aug2920171550/2VectorProcessing/Stream10000_sln30_xln_Decomp.shp' )
+
+
+for ( i  in 1:Output.Project.river.info$nrows) {
+  
+  Output.Project.river@lines[[i]]@ID<-as.character(i)
+}
+
+
+
+##### plot the shape File
+
+plot(Output.Project.mesh, col="light green",lwd=2);
+lines(Output.Project.river,col="BLUE",lwd=3) ;
+title("Output.Project MESH") ;
+
+
+####### read the dataframe with the attributes collected from the ground water output obtaqined from pihm
+
+# ########## Identify the output runs that are in the output file
+# 
+# PIHM.runs<-list.files(path=".\\output")   ;
+# 
+# length(PIHM.runs) ;
+# 
+# print(PIHM.runs)  ;
+# 
+
+##########################################Read the ground water table data ####################################
+
+###############################################################################################################
+#                      Read binary files 
+###############################################################################################################
+
+
+list.files("./")   ;
+
+file_info<-file.info(paste0('./',Output.Project,'/', 'HansYoust.gw.dat'))   ; # information about the binary file
+
+word.size=8 ;  # eight bytes per word
+
+Triangle.no=NumEle ; # number of triangles in the output simulation
+
+No.Rows<-file_info$size/word.size/(Triangle.no+1) ; # number of rows obtained by dividing the file size in bytes
+# by wordsize*(Tiangle.no +1 (time stamp))  
+
+Read_data<-readBin(paste0('./',Output.Project,'/', 'HansYoust.recharge.dat') , "numeric" , size=word.size, n=file_info$size/word.size) ; # read the binary filw
+
+GW<-as.data.frame(matrix(data=Read_data, ncol=Triangle.no+1, byrow=T)); # create a matrix with the binary file
+# and then transforme it into a dataframe.
+
+
+GW$Time<-as.POSIXct(GW[,1], origin="1970-01-01", tz="UTC") ;
+
+
+length(GW$Time)
+
+#### Select a time Range that it is interesting to explore
+
+DateTime.range<-c(GW$Time[1],GW$Time[length(GW$Time)]); #Based on a specific range of data that one might want to explore
+
+
+Rows.Time.range<-which(GW$Time >= DateTime.range[1] & GW$Time <= DateTime.range[2] ); 
+Columns.Time.range<-which(GW$Time >= DateTime.range[1] & GW$Time <= DateTime.range[2] ); 
+
+# Only select columns 2 (the first is the TS) to 242+1 (the number of mesh elemnts), the rest is the channel ground water storage (32 elements)
+
+GW.Land<-GW[Rows.Time.range,2:(Output.Project.mesh.info$nrows+1)] ;
+
+
+
+
+# Find the largest and smaller values in the data set for the land segments
+
+GW.Land.range<-range(GW.Land) ;
+
+
+# plot and explore the ground water time series   
+plot(GW$Time[Rows.Time.range],GW.Land[,1],type="l", ylim=GW.Land.range);
+
+for (i in seq(2,dim(GW.Land)[2])) {
+  points(GW$Time[Rows.Time.range],GW.Land[,i],type='l',col=i ) ;
+} ;
+
+
+
+####### attach the GW data to the shape file as attibutes
+
+
+GW.TimeSeries<-as.data.frame(t(WE38.GW.Land))
+
+row.names(GW.TimeSeries)<-seq(1,dim(GW.TimeSeries)[1]) ;
+
+
+WE38.GW.spdf<-SpatialPolygonsDataFrame(WE38.mesh,GW.TimeSeries, match.ID = T);
+
+###### Name the columns of the attribute data frame according to the date range
+
+names(WE38.GW.spdf@data)<-c(as.character(DateTime[Rows.Time.range]),"polygonID")
+
+DateTime[Rows.Time.range]
+
+
+###### Get the Polygons IDs and labeling coordinates
+
+WE38.ID<-sapply(slot(WE38.GW.spdf,"polygons"),function(x) slot(x,"ID")) ;
+WE38.labpt<-sapply(slot(WE38.GW.spdf,"polygons"),function(x) slot(x,"labpt")) ;
+
+
+
+# #  Plot the Spatial polygonDataFrame with the Lattice package
+# 
+# plot(WE38.GW.spdf);
+# text(coordinates(t(WE38.labpt)),WE38.ID);
+# 
+# Colors.in.map<-brewer.pal(12,"Set3")
+# spplot(WE38.GW.spdf,Columns,main="WE38 Soil Water");
+
+
+
+
+#################### using the Package tmap to plot the spatial data
+
+##################### creating a color pallete that shows more than 12 colors
+
+color.palette<- colorRampPalette(c("yellow","green", "blue"), space="rgb");
+pie(rep(1,40),col=color.palette (40) )  ;
+
+
+#####   Plotting the map
+
+####GEt the Polygon ID 
+WE38.GW.spdf$polygonID<-sapply(slot(WE38.GW.spdf,"polygons"),function(x) slot(x,"ID")) ;
+
+#### Plotting the polygon shape file using the tmap
+
+qtm(shp=WE38.GW.spdf, fill="2008-03-04", fill.palette=colorRampPalette(c("yellow","green", "blue"), space="rgb")(5),text="polygonID"); 
+
+WE38.river$LineID<-sapply(slot(WE38.river,"lines"),function(x) slot(x,"ID"))  ;
+
+tm_shape(WE38.GW.spdf) +
+  tm_fill("2008-03-04", palette=colorRampPalette(c("yellow","green", "blue"), space="rgb")(11), n=10 )+ 
+  #     tm_text("polygonID")
+  tm_shape(WE38.river) + ##########Plot the river and other lines
+  tm_lines(col="blue",lwd=3)
+#     tm_text("LineID")
+
+
+############## Plotting a series of maps for a rain storm simulation  ###########
+
+i<-as.character(DateTime[Rows.Time.range]) [14]
+
+tm_shape(WE38.GW.spdf)+
+  tm_borders(col = "Black", lwd = 1, lty = "solid", alpha = NA) +   
+  tm_fill(i, palette=colorRampPalette(c("lightblue", "blue","darkblue"), space="rgb")(9))+ 
+  #     tm_text("polygonID")
+  tm_shape(WE38.river) + ##########Plot the river and other lines
+  tm_lines(col="blue",lwd=3)
+#     tm_text("LineID")
+
+
+
+
+
+
+#################  Plotting  surface flow to stream from left terrain .rivFlx2   #################################
+
+##########    Read the .rivFlx2  data ######### 
+
+WE38.rivFlx2<-read.table(paste0(".\\output","\\",PIHM.runs[1],"\\","WE38.rivFlx2.txt"),header=F,as.is=T) ;
+
+dim(WE38.rivFlx2)
+
+# Find the largest and smaller values in the data set 
+
+WE38.rivFlx2.range<-range(WE38.rivFlx2[,seq(2,dim(WE38.rivFlx2)[2])]) ;
+
+
+# plot and explore .rivFlx2  time series   
+plot(DateTime,WE38.rivFlx2[,2],type="l",ylim=WE38.rivFlx2.range);
+
+for (i in seq(3,dim(WE38.rivFlx2)[2])) {
+  points(DateTime,WE38.rivFlx2[,i],type='l',col=i ) ;
+} ;
+
+
+##########    Read the .rivFlx4  data ######### 
+
+WE38.rivFlx4<-read.table(paste0(".\\output","\\",PIHM.runs[1],"\\","WE38.rivFlx4.txt"),header=F,as.is=T) ;
+
+dim(WE38.rivFlx4)
+
+# Find the largest and smaller values in the data set 
+
+WE38.rivFlx4.range<-range(WE38.rivFlx4[,seq(2,dim(WE38.rivFlx4)[2])]) ;
+
+
+# plot and explore .rivFlx4  time series   
+plot(DateTime,WE38.rivFlx4[,2],type="l",ylim=WE38.rivFlx4.range);
+
+for (i in seq(3,dim(WE38.rivFlx4)[2])) {
+  points(DateTime,WE38.rivFlx4[,i],type='l',col=i ) ;
+} ;
+
+
+
+##########    Read the .rivFlx6  data ######### 
+
+WE38.rivFlx6<-read.table(paste0(".\\output","\\",PIHM.runs[1],"\\","WE38.rivFlx6.txt"),header=F,as.is=T) ;
+
+dim(WE38.rivFlx6)
+
+# Find the largest and smaller values in the data set 
+
+WE38.rivFlx6.range<-range(WE38.rivFlx6[,seq(2,dim(WE38.rivFlx6)[2])]) ;
+
+
+# plot and explore .rivFlx6  time series   
+plot(DateTime,WE38.rivFlx6[,2],type="l",ylim=WE38.rivFlx6.range);
+
+for (i in seq(3,dim(WE38.rivFlx6)[2])) {
+  points(DateTime,WE38.rivFlx6[,i],type='l',col=i ) ;
+} ;
+
+
+
+##########    Read the .rivFlx7  data ######### 
+
+WE38.rivFlx7<-read.table(paste0(".\\output","\\",PIHM.runs[1],"\\","WE38.rivFlx7.txt"),header=F,as.is=T) ;
+
+dim(WE38.rivFlx7)
+
+# Find the largest and smaller values in the data set 
+
+WE38.rivFlx7.range<-range(WE38.rivFlx7[,seq(2,dim(WE38.rivFlx7)[2])]) ;
+
+
+# plot and explore .rivFlx7  time series   
+plot(DateTime,WE38.rivFlx7[,2],type="l",ylim=WE38.rivFlx7.range);
+
+for (i in seq(3,dim(WE38.rivFlx7)[2])) {
+  points(DateTime,WE38.rivFlx7[,i],type='l',col=i ) ;
+} ;
 
 
